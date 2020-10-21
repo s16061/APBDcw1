6ba4443301e76d389dac127f6b6669a82aa59f9d
Michał Karczmarczyk <michalkarczmarczyk1@gmail.com>
2020-10-21 18:32:04 +0200

    Dodaj pliki projektów.

---
+++ APBDcw1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace APBDcw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
