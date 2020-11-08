d4bb9b481641a81eb69bfffe7886d6e4b83468fd
Michał Karczmarczyk <s16061@pjwstk.edu.pl>
2020-11-08 12:44:51 +0100

    Zad 1 update

--- APBDcw1/Program.cs
+++ APBDcw1/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace APBDcw1
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var url = "https://www.pja.edu.pl";
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(url);
+
+            if(response.IsSuccessStatusCode)
+            {
+                var html = await response.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z0-9]+@[a-z.]+");
+
+                MatchCollection matches = regex.Matches(html);
+                foreach (var i in matches)
+                    Console.WriteLine(i);
+            }
+        
         }
     }
 }
